3575d37a4d77a25041afcd104ff189bf608fc3a8
amanrajaofficialhelp <amanrajaofficialhelp@gmail.com>
2026-02-04 13:04:46 +0530

    firebase and notifee setup done with commented code

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 /**
  * @format
  */
-
 import { AppRegistry } from 'react-native';
 import App from './src/app/App';
 import { name as appName } from './app.json';
+// import { foregroundMessage, backgroundMessageHandler } from './src/shared/services/firebase/notification'
+// import { setupNotificationListeners } from './src/shared/services/notifee/notifee';
+
+// foregroundMessage()
+// backgroundMessageHandler()
+// setupNotificationListeners()
 
 AppRegistry.registerComponent(appName, () => App);
